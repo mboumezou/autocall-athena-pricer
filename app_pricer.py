3a1739c798fd7f04b00edfa24603120df48c3f42
Mohamed Boumezou <boumezou29@gmail.com>
2025-05-28 11:34:08 +0200

    Update app_pricer.py

--- app_pricer.py
+++ app_pricer.py
@@ -169,14 +169,7 @@ def run_backtest(stock_symbol, start_date, end_date, T, z, lambda_barrier, S0):
 # %% Application Streamlit
 
 st.set_page_config(
-    layout="wide",
-    page_title="Mon Autocall Athena",
-    page_icon="📈",
-    menu_items={
-        'Get Help': None,
-        'Report a Bug': None,
-        'About': None
-    }
+    layout="wide"
 )
 
 
